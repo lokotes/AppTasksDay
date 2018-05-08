5c3632656bd81a73e632d0b60cf01583b845cae4
diegoferrari <diegoferraricamus@gmail.com>
2018-05-08 17:23:58 -0500

    Primera Version

---
+++ AppTasksDay/Tarea.swift
@@ -0,0 +1,23 @@
+//
+//  Tarea.swift
+//  AppTasksDay
+//
+//  Created by MacAMP on 8/05/18.
+//  Copyright © 2018 MacAMP. All rights reserved.
+//
+
+import Foundation
+
+class Tarea
+{
+    //var idusuario: Int
+    var idtarea: Int
+    var descripcion: String
+    var estado: String
+    
+    init(_ idtarea: Int,_ descripcion: String,_ estado: String) {
+        self.idtarea=idtarea
+        self.descripcion=descripcion
+        self.estado=estado
+    }
+}
